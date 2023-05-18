a45a37f651b7ca5066484f983c4e97e7e427bb1c
Vedanta Kaushik <vedantakaushik0@yahoo.com>
2023-05-18 17:03:11 +0530

    18-05-2023

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -55,6 +55,7 @@ export const LoginAUser = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 86400000,
+        sameSite: "none",
       })
       .json({ sucess: true, user, _id });
   } catch (error) {
